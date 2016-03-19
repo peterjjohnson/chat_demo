7266e1a284079e78fde219f3e35d542fcf43df5f
silenceandshadow <pjohnson@cluricon.com>
2016-03-19 15:34:08 -0400

    Just a little tidying up

--- client/footer.js
+++ client/footer.js
@@ -1,11 +1,11 @@
 Template.footer.events({
     'keypress input': function(event) {
         var inputBox = $('.input-box_text');
-        var charAscii = (typeof event.which == "number") ? event.which : event.charCode;
+        var charAscii = (typeof event.which == 'number') ? event.which : event.charCode;
         if (charAscii == 13) {
             event.stopPropagation();
             Meteor.call('newMessage', {text: inputBox.val()});
-            inputBox.val("");
+            inputBox.val('');
             return false;
         }
     }
