9ab893768f4c48637c9311f3e16e822830edf97d
silenceandshadow <pjohnson@cluricon.com>
2016-03-31 01:42:51 -0400

    Baby steps...

--- client/app.js
+++ client/app.js
@@ -1,3 +1,5 @@
+var decryptionReady = false;
+
 function byteArrayToBase64(byteArray){
     var binaryString = "";
     for (var i = 0; i < byteArray.byteLength; i++){
@@ -48,26 +50,31 @@ function keyToBase64(key) {
 }
 
 function encrypt(message) {
-    window.crypto.subtle.importKey(
-        'spki',
-        base64ToByteArray(Meteor.call('getPublicKey', message.recipient)),
-        {name: 'RSA-OAEP', hash: 'SHA-256'},
-        false,
-        ['encrypt']
-    ).then(function(publicKey) {
-        window.crypto.subtle.encrypt(
-            {name: 'RSA-OAEP'},
-            publicKey,
-            message.text
-        ).then(function(cipherText) { console.log(cipherText);
-            message.text = byteArrayToBase64(new Uint8Array(cipherText));
-            Meteor.call('newMessage', message);
-        });
+    Meteor.call('getPublicKey', message.recipient, function(err, key) {
+        if (err) {
+            alert('error');
+        } else {
+            window.crypto.subtle.importKey(
+                'spki',
+                base64ToByteArray(key),
+                {name: 'RSA-OAEP', hash: 'SHA-256'},
+                false,
+                ['encrypt']
+            ).then(function(publicKey) {
+                window.crypto.subtle.encrypt(
+                    {name: 'RSA-OAEP'},
+                    publicKey,
+                    stringToByteArray(message.text)
+                ).then(function(cipherText) {
+                    message.text = byteArrayToBase64(new Uint8Array(cipherText));
+                    Meteor.call('newMessage', message);
+                });
+            });
+        }
     });
 }
 
 function decrypt(message) {
-    console.log(message);
     window.crypto.subtle.importKey(
         'pkcs8',
         base64ToByteArray($('#private-key').val()),
@@ -79,8 +86,10 @@ function decrypt(message) {
             {name: 'RSA-OAEP'},
             privateKey,
             base64ToByteArray(message.text)
-        ).then(function(plainText) {
-            message.text = new Uint8Array(plainText);
+        ).then(function(decryptedText) {
+            $('#' + message._id).text(byteArrayToString(decryptedText));
+        }).catch(function(error) {
+            console.log('fuck');
         });
     });
 }
@@ -103,22 +112,11 @@ function generateKeyPair() {
         });
         window.crypto.subtle.exportKey(
             'pkcs8', keyPair.privateKey
-        ).then(function (privateKey) {
+        ).then(function(privateKey) {
             $('#private-key').val(keyToBase64(privateKey));
         });
     });
 }
-
-/**
- * Helper function for messages template to return a list of messages
- */
-Template.messages.helpers({
-    messages: function() {
-        var messages = Messages.find();
-        messages.forEach(decrypt);
-        return messages;
-    }
-});
 
 /**
  * Helper function to return a username when provided with a userId
@@ -137,6 +135,11 @@ Template.registerHelper('usernameFromId', function(userId) {
     return username;
 });
 
+Template.registerHelper('publicKeyFromUserId', function(userId) {
+    var user = Meteor.users.findOne({_id: userId}, {fields: {publicKey: 1}});
+    return user.publicKey;
+});
+
 /**
  * Given a timestamp, return a time string in the format hh:mm:ss
  */
@@ -147,6 +150,16 @@ Template.registerHelper('timestampToTime', function(timestamp) {
     var seconds = '0' + date.getSeconds();
 
     return hours + ':' + minutes.substr(minutes.length - 2) + ':' + seconds.substr(seconds.length - 2);
+});
+
+/**
+ * Helper function for messages template to return a list of messages
+ */
+Template.messages.helpers({
+    messages: function() {
+        var messages = Messages.find();
+        return messages;
+    }
 });
 
 /**
@@ -180,10 +193,13 @@ Template.footer.events({
         if (charAscii == 13) {
             event.stopPropagation();
             encrypt({text: inputBox.val(), recipient: Session.get('userId')});
-            //Meteor.call('newMessage', {text: inputBox.val(), recipient: Session.get('userId')});
             inputBox.val('');
             return false;
         }
+    },
+    'click #decrypt-messages': function() {
+        var messages = Messages.find();
+        messages.forEach(decrypt);
     }
 });
 
@@ -192,8 +208,8 @@ Accounts.ui.config({
 });
 
 Accounts.onLogin(function() {
-    var publicKey = Meteor.call('getPublicKey', Meteor.userId());
-    if (!publicKey) {
+    var publickey = Meteor.call('getPublicKey', Meteor.userId());
+    if (publicKey === undefined) {
         generateKeyPair();
     }
 });
